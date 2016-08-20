ab9c729897a1fb70f4e2692403b3760d834b27c3
Shreyas S <hello@shreyas.im>
2016-08-20 15:46:25 +0530

    Mailers Setup Complete.
    Bugs in bookings -
    1. same date different month
    2. minimum days
    3. date before today

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
    :domain => "gmail.com",
    :authentication => "plain",
    :enable_starttls_auto => true,
-   :user_name => "nsudhanva@gmail.com",
-   :password => "Sudhanva@sud*9",
+   :user_name => "hello@shreyas.im",
+   :password => "8050Zxcvb",
    :openssl_verify_mode  => 'none'
 }
